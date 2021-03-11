f0a37b01bce7543e12babb672df7b0788400d046
rschouten97 <r.schouten@tools4ever.com>
2021-03-11 09:53:18 +0100

    Updated script to switch between 'use assignments'

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # HelloID-Conn-Prov-Source-RAET-IAM-API
 
 This connector retrieves HR data from the RAET IAM API. Please be aware that there are several versions. This version connects to the latest API release.
+
+
+Please Note! The mapping examples are currently only an example when assignments are NOT used
--- persons.ps1
+++ persons.ps1
@@ -3,6 +3,7 @@ $config = $configuration | ConvertFrom-Json
 $clientId = $config.connection.clientId
 $clientSecret = $config.connection.clientSecret
 $tenantId = $config.connection.tenantId
+$includeAssignments = $config.switchIncludeAssignments
 
 function New-RaetSession { 
     [CmdletBinding()]
@@ -115,7 +116,7 @@ function Get-RaetPersonDataList {
     
     try {
         $persons = Invoke-RaetWebRequestList -Url "$Script:BaseUrl/employees"
-        
+
         # Make sure persons are unique
         $persons = $persons | Sort-Object id -Unique
 
@@ -123,20 +124,22 @@ function Get-RaetPersonDataList {
         $jobProfiles = $jobProfiles | Select-Object * -ExcludeProperty extensions
         $jobProfileGrouped = $jobProfiles | Group-Object Id -AsHashTable
 
-        $assignments = Invoke-RaetWebRequestList -Url "$Script:BaseUrl/assignments"
-        #$assignmentsGrouped = $assignments | Select-Object -Property @{Name = "employmentRelationId"; Expression = { $_.personCode + "_" + $_.employmentCode } }, * | Group-Object employmentRelationId -AsHashTable
+        if($true -eq $includeAssignments){
+            $assignments = Invoke-RaetWebRequestList -Url "$Script:BaseUrl/assignments"
 
-        $assignmentHashtable = @{}
-        foreach ($record in $assignments) {
-            $tmpKey = $record.personCode + "_" + $record.employmentCode
-            if (![string]::IsNullOrEmpty($tmpKey)) {
-                if($assignmentHashtable.Contains($tmpKey)) {
-                    $assignmentHashtable.$tmpKey += ($record)
-                } else {
-                    $assignmentHashtable.Add($tmpKey, @($record))
-                } 
-            }
-        }         
+            $assignmentHashtable = @{}
+            foreach ($record in $assignments) {
+                $tmpKey = $record.personCode + "_" + $record.employmentCode
+                if (![string]::IsNullOrEmpty($tmpKey)) {
+                    if($assignmentHashtable.Contains($tmpKey)) {
+                        $assignmentHashtable.$tmpKey += ($record)
+                    } else {
+                        $assignmentHashtable.Add($tmpKey, @($record))
+                    } 
+                }
+            }   
+        }
+      
 
         # Extend the persons model
         $persons | Add-Member -MemberType NoteProperty -Name "BusinessEmailAddress" -Value $null -Force
@@ -153,48 +156,83 @@ function Get-RaetPersonDataList {
                 $displayName = ($person.knownAs + ' ' + $person.lastNameAtBirth)
                 $person | Add-Member -Name "DisplayName" -MemberType NoteProperty -Value $displayName
                                                             
-                $contracts = New-Object System.Collections.Generic.List[System.Object]                  
-                foreach ($employment in $person.employments) {
-                    $lookingFor = $person.personCode + "_" + $employment.employmentCode
-                    #$personAssignments = $assignmentsGrouped[$person.personCode + "_" + $employment.employmentCode]
+                $contracts = New-Object System.Collections.Generic.List[System.Object]
+                foreach ($employment in $person.employments) {                
+                    $jobProfile = $jobProfileGrouped["$($employment.jobProfile)"]    
+                    
+                    if($true -eq $includeAssignments){
+                        # Create Contract object(s) based on assignments
+                        $lookingFor = $person.personCode + "_" + $employment.employmentCode
+                        #$personAssignments = $assignmentsGrouped[$person.personCode + "_" + $employment.employmentCode]
 
-                    $personAssignments = $assignmentHashtable.$lookingFor
-                    foreach($assignment in $personAssignments){
-                        if ($assignment.employmentCode -eq $employment.employmentCode) {
-                            $jobProfile = $jobProfileGrouped["$($assignment.jobProfile)"]                                                                                    
-                                
-                            #Contract result object used in HelloID
-                            $Contract = [PSCustomObject]@{
-                                #ExternalId       = $person.personCode + '_' + $employment.employmentCode + '_' + $assignment.employmentCode
-                                ExternalId       = $assignment.id
-                                EmploymentType   = @{
-                                    ShortName = $employment.employmentType
-                                    FullName  = $null
+                        $personAssignments = $assignmentHashtable.$lookingFor
+                        foreach($assignment in $personAssignments){
+                            if ($assignment.employmentCode -eq $employment.employmentCode) {
+                                $jobProfile = $jobProfileGrouped["$($assignment.jobProfile)"]                                                                                    
+                                    
+                                #Contract result object used in HelloID
+                                $Contract = [PSCustomObject]@{
+                                    ExternalId       = $assignment.id
+                                    EmploymentType   = @{
+                                        ShortName = $employment.employmentType
+                                        FullName  = $null
+                                    }
+                                    PersonCode       = $person.personCode
+                                    EmploymentCode   = $employment.employmentCode
+                                    StartDate        = $assignment.startDate
+                                    EndDate          = $assignment.endDate
+                                    DischargeDate    = $employment.dischargeDate
+                                    HireDate         = $employment.hireDate
+                                    JobProfile       = @{
+                                        ShortName = $assignment.jobProfile
+                                        FullName  = $($jobProfile.fullName)
+                                    }
+                                    WorkingAmount    = @{
+                                        AmountOfWork = $assignment.workingAmount.amountOfWork
+                                        UnitOfWork   = $assignment.workingAmount.unitOfWork
+                                        PeriodOfWork = $assignment.workingAmount.periodOfWork
+                                    }
+                                    OrganizationUnit = @{
+                                        ShortName = $assignment.organizationUnit
+                                        FullName  = $null
+                                    }
                                 }
-                                PersonCode       = $person.personCode
-                                EmploymentCode   = $employment.employmentCode
-                                StartDate        = $assignment.startDate
-                                EndDate          = $assignment.endDate
-                                DischargeDate    = $employment.dischargeDate
-                                HireDate         = $employment.hireDate
-                                JobProfile       = @{
-                                    ShortName = $assignment.jobProfile
-                                    FullName  = $($jobProfile.fullName)
-                                }
-                                WorkingAmount    = @{
-                                    AmountOfWork = $assignment.workingAmount.amountOfWork
-                                    UnitOfWork   = $assignment.workingAmount.unitOfWork
-                                    PeriodOfWork = $assignment.workingAmount.periodOfWork
-                                }
-                                OrganizationUnit = @{
-                                    ShortName = $assignment.organizationUnit
-                                    FullName  = $null
-                                }
+                                $contracts.add($Contract)
                             }
-                            $contracts.add($Contract)
+                        } 
+                    }else{
+                        # Create Contract object(s) based on employments
+
+                        #Contract result object used in HelloID
+                        $Contract = [PSCustomObject]@{
+                            ExternalId       = $person.personCode + '_' + $employment.employmentCode
+                            EmploymentType   = @{
+                                ShortName = $employment.employmentType
+                                FullName  = $null
+                            }
+                            PersonCode       = $person.personCode
+                            EmploymentCode   = $employment.employmentCode
+                            StartDate        = $employment.hireDate
+                            EndDate          = $employment.dischargeDate
+                            DischargeDate    = $employment.dischargeDate
+                            HireDate         = $employment.hireDate
+                            JobProfile       = @{
+                                ShortName = $employment.jobProfile
+                                FullName  = $($jobProfile.fullName)
+                            }
+                            WorkingAmount    = @{
+                                AmountOfWork = $employment.workingAmount.amountOfWork
+                                UnitOfWork   = $employment.workingAmount.unitOfWork
+                                PeriodOfWork = $employment.workingAmount.periodOfWork
+                            }
+                            OrganizationUnit = @{
+                                ShortName = $employment.organizationUnit
+                                FullName  = $null
+                            }
                         }
-                    } 
- 
+                        $contracts.add($Contract)
+                    }
+                                                                                
                     $person | Add-Member -Name "Contracts" -MemberType NoteProperty -Value $contracts -Force
 
                     # Add emailAddresses to the person
@@ -229,9 +267,30 @@ function Get-RaetPersonDataList {
                         $person | Add-Member -Name $person.extensions.key -MemberType NoteProperty -Value $person.extensions.value -Force
                     }
                 }
+
+                # Convert naming convention codes to standard
+                Switch($person.nameAssembleOrder ){
+                    "E" {
+                        $person.nameAssembleOrder = "B"
+                    }
+                    "P" {
+                        $person.nameAssembleOrder = "P"
+                    }
+                    "C" {
+                        $person.nameAssembleOrder = "BP"
+                    }
+                    "B" {
+                        $person.nameAssembleOrder = "PB"
+                    }
+                    "D" {
+                        $person.nameAssembleOrder = "BP"
+                    }
+                }
+
                 Write-Output $person | ConvertTo-Json -Depth 10
             }
         }
+        Write-Verbose -Verbose "Persons import completed: $($persons.count)"
     }
     catch {
         Throw "Could not Get-RaetPersonDataList, message: $($_.Exception.Message)"
