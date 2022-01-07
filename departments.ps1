45a9dfb3ff4f5a2673df9d34f9dddc759a4eefbc
r.jongbloed@tools4ever.com <r.jongbloed@tools4ever.com>
2022-01-07 11:43:24 +0100

    Whitespace and ; removal

--- departments.ps1
+++ departments.ps1
@@ -1,30 +1,30 @@
-$config = $configuration | ConvertFrom-Json 
+$config = $configuration | ConvertFrom-Json
 
 $clientId = $config.connection.clientId
 $clientSecret = $config.connection.clientSecret
 $tenantId = $config.connection.tenantId
 
-function New-RaetSession { 
+function New-RaetSession {
     [CmdletBinding()]
     param (
-        [Alias("Param1")] 
-        [parameter(Mandatory = $true)]  
-        [string]      
+        [Alias("Param1")]
+        [parameter(Mandatory = $true)]
+        [string]
         $ClientId,
 
-        [Alias("Param2")] 
-        [parameter(Mandatory = $true)]  
+        [Alias("Param2")]
+        [parameter(Mandatory = $true)]
         [string]
         $ClientSecret,
 
-        [Alias("Param3")] 
-        [parameter(Mandatory = $false)]  
+        [Alias("Param3")]
+        [parameter(Mandatory = $false)]
         [string]
         $TenantId
     )
-   
+
     #Check if the current token is still valid
-    if (Confirm-AccessTokenIsValid -eq $true) {       
+    if (Confirm-AccessTokenIsValid -eq $true) {
         return
     }
 
@@ -33,9 +33,9 @@ function New-RaetSession {
         'grant_type'    = "client_credentials"
         'client_id'     = $ClientId
         'client_secret' = $ClientSecret
-    } 
+    }
     try {
-        [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+        [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
         $result = Invoke-WebRequest -Uri $url -Method Post -Body $authorisationBody -ContentType 'application/x-www-form-urlencoded' -Headers @{'Cache-Control' = "no-cache" } -Proxy:$Proxy -UseBasicParsing
         $accessToken = $result.Content | ConvertFrom-Json
         $Script:expirationTimeAccessToken = (Get-Date).AddSeconds($accessToken.expires_in)
@@ -44,33 +44,33 @@ function New-RaetSession {
             'X-Client-Id'      = $ClientId;
             'Authorization'    = "Bearer $($accessToken.access_token)";
             'X-Raet-Tenant-Id' = $TenantId;
-           
-        }     
+
+        }
     } catch {
         if ($_.Exception.Response.StatusCode -eq "Forbidden") {
             $errorMessage = "Something went wrong $($_.ScriptStackTrace). Error message: '$($_.Exception.Message)'"
         } elseif (![string]::IsNullOrEmpty($_.ErrorDetails.Message)) {
-            $errorMessage = "Something went wrong $($_.ScriptStackTrace). Error message: '$($_.ErrorDetails.Message)'" 
+            $errorMessage = "Something went wrong $($_.ScriptStackTrace). Error message: '$($_.ErrorDetails.Message)'"
         } else {
-            $errorMessage = "Something went wrong $($_.ScriptStackTrace). Error message: '$($_)'" 
-        }  
+            $errorMessage = "Something went wrong $($_.ScriptStackTrace). Error message: '$($_)'"
+        }
         throw $errorMessage
-    } 
+    }
 }
 
 function Confirm-AccessTokenIsValid {
     if ($null -ne $Script:expirationTimeAccessToken) {
         if ((Get-Date) -le $Script:expirationTimeAccessToken) {
             return $true
-        }        
+        }
     }
-    return $false    
+    return $false
 }
 
 function Invoke-RaetWebRequestList {
     [CmdletBinding()]
     param (
-        [parameter(Mandatory = $true)]  
+        [parameter(Mandatory = $true)]
         [string]
         $Url
     )
@@ -82,11 +82,11 @@ function Invoke-RaetWebRequestList {
         }
 
         [System.Collections.ArrayList]$ReturnValue = @()
-        $counter = 0 
+        $counter = 0
         do {
             if ($counter -gt 0) {
                 $SkipTakeUrl = $resultSubset.nextLink.Substring($resultSubset.nextLink.IndexOf("?"))
-            }    
+            }
             $counter++
             [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
             $result = Invoke-WebRequest -Uri $Url$SkipTakeUrl -Method GET -ContentType "application/json" -Headers $Script:AuthenticationHeaders -UseBasicParsing
@@ -98,17 +98,16 @@ function Invoke-RaetWebRequestList {
         if ($_.Exception.Response.StatusCode -eq "Forbidden") {
             $errorMessage = "Something went wrong $($_.ScriptStackTrace). Error message: '$($_.Exception.Message)'"
         } elseif (![string]::IsNullOrEmpty($_.ErrorDetails.Message)) {
-            $errorMessage = "Something went wrong $($_.ScriptStackTrace). Error message: '$($_.ErrorDetails.Message)'" 
+            $errorMessage = "Something went wrong $($_.ScriptStackTrace). Error message: '$($_.ErrorDetails.Message)'"
         } else {
-            $errorMessage = "Something went wrong $($_.ScriptStackTrace). Error message: '$($_)'" 
-        }  
+            $errorMessage = "Something went wrong $($_.ScriptStackTrace). Error message: '$($_)'"
+        }
         throw $errorMessage
     }
     return $ReturnValue
 }
 
-function Get-RaetOrganizationUnitsList { 
-   
+function Get-RaetOrganizationUnitsList {
     $Script:BaseUrl = "https://api.raet.com/iam/v1.0"
 
     try {
@@ -129,7 +128,7 @@ function Get-RaetOrganizationUnitsList {
                     $roleAssignment
                 }
             }
-            
+
             $managerId = $null
             $ExternalIdOu = $item.id
             foreach ($roleAssignment in $ouRoleAssignments) {
@@ -137,14 +136,14 @@ function Get-RaetOrganizationUnitsList {
                     if ($roleAssignment.ShortName -eq 'MGR') {
                         $startDate = ([Datetime]::ParseExact($roleAssignment.startDate, 'yyyy-MM-dd', $null))
                         $endDate = ([Datetime]::ParseExact($roleAssignment.endDate, 'yyyy-MM-dd', $null))
-                      
+
                         if($managerActiveCompareDate -ge $startDate -and $endDate -ge $managerActiveCompareDate ){
                             $managerId = $roleAssignment.personCode
                             break
                         }
                     }
                 }
-            }   
+            }
 
             $organizationUnit = [PSCustomObject]@{
                 ExternalId=$ExternalIdOu
@@ -155,11 +154,10 @@ function Get-RaetOrganizationUnitsList {
             }
             $null = $departments.Add($organizationUnit)
         }
-        Write-Verbose -Verbose "Department import completed";
-        Write-Output $departments | ConvertTo-Json -Depth 10;
-        
+        Write-Verbose -Verbose "Department import completed"
+        Write-Output $departments | ConvertTo-Json -Depth 10
     } catch {
-        throw "Could not Get-OrganizationUnitsList, message: $($_.Exception.Message)"   
+        throw "Could not Get-OrganizationUnitsList, message: $($_.Exception.Message)"
     }
 }
 
--- persons.ps1
+++ persons.ps1
@@ -1,31 +1,31 @@
-$config = $configuration | ConvertFrom-Json 
+$config = $configuration | ConvertFrom-Json
 
 $clientId = $config.connection.clientId
 $clientSecret = $config.connection.clientSecret
 $tenantId = $config.connection.tenantId
 $includeAssignments = $config.switchIncludeAssignments
 
-function New-RaetSession { 
+function New-RaetSession {
     [CmdletBinding()]
     param (
-        [Alias("Param1")] 
-        [parameter(Mandatory = $true)]  
-        [string]      
+        [Alias("Param1")]
+        [parameter(Mandatory = $true)]
+        [string]
         $ClientId,
 
-        [Alias("Param2")] 
-        [parameter(Mandatory = $true)]  
+        [Alias("Param2")]
+        [parameter(Mandatory = $true)]
         [string]
         $ClientSecret,
 
-        [Alias("Param3")] 
-        [parameter(Mandatory = $false)]  
+        [Alias("Param3")]
+        [parameter(Mandatory = $false)]
         [string]
         $TenantId
     )
-   
+
     #Check if the current token is still valid
-    if (Confirm-AccessTokenIsValid -eq $true) {       
+    if (Confirm-AccessTokenIsValid -eq $true) {
         return
     }
 
@@ -34,56 +34,55 @@ function New-RaetSession {
         'grant_type'    = "client_credentials"
         'client_id'     = $ClientId
         'client_secret' = $ClientSecret
-    } 
+    }
     try {
-        [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+        [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
         $result = Invoke-WebRequest -Uri $url -Method Post -Body $authorisationBody -ContentType 'application/x-www-form-urlencoded' -Headers @{'Cache-Control' = "no-cache" } -Proxy:$Proxy -UseBasicParsing
         $accessToken = $result.Content | ConvertFrom-Json
         $Script:expirationTimeAccessToken = (Get-Date).AddSeconds($accessToken.expires_in)
 
         $Script:AuthenticationHeaders = @{
-            'X-Client-Id'      = $ClientId;
-            'Authorization'    = "Bearer $($accessToken.access_token)";
-            'X-Raet-Tenant-Id' = $TenantId;
-           
-        }     
+            'X-Client-Id'      = $ClientId
+            'Authorization'    = "Bearer $($accessToken.access_token)"
+            'X-Raet-Tenant-Id' = $TenantId
+        }
     }
     catch {
         if ($_.Exception.Response.StatusCode -eq "Forbidden") {
             $errorMessage = "Something went wrong $($_.ScriptStackTrace). Error message: '$($_.Exception.Message)'"
         } elseif (![string]::IsNullOrEmpty($_.ErrorDetails.Message)) {
-            $errorMessage = "Something went wrong $($_.ScriptStackTrace). Error message: '$($_.ErrorDetails.Message)'" 
+            $errorMessage = "Something went wrong $($_.ScriptStackTrace). Error message: '$($_.ErrorDetails.Message)'"
         } else {
-            $errorMessage = "Something went wrong $($_.ScriptStackTrace). Error message: '$($_)'" 
-        }  
+            $errorMessage = "Something went wrong $($_.ScriptStackTrace). Error message: '$($_)'"
+        }
         throw $errorMessage
-    } 
+    }
 }
 
 function Confirm-AccessTokenIsValid {
     if ($null -ne $Script:expirationTimeAccessToken) {
         if ((Get-Date) -le $Script:expirationTimeAccessToken) {
             return $true
-        }        
+        }
     }
-    return $false    
+    return $false
 }
 
 function Invoke-RaetWebRequestList {
     [CmdletBinding()]
     param (
-        [parameter(Mandatory = $true)]  
+        [parameter(Mandatory = $true)]
         [string]
         $Url
     )
     try {
 
         [System.Collections.ArrayList]$ReturnValue = @()
-        $counter = 0 
+        $counter = 0
         do {
             if ($counter -gt 0) {
                 $SkipTakeUrl = $resultSubset.nextLink.Substring($resultSubset.nextLink.IndexOf("?"))
-            }    
+            }
             $counter++
             [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
             $accessTokenValid = Confirm-AccessTokenIsValid
@@ -93,27 +92,27 @@ function Invoke-RaetWebRequestList {
             $result = Invoke-WebRequest -Uri $Url$SkipTakeUrl -Method GET -ContentType "application/json" -Headers $Script:AuthenticationHeaders -UseBasicParsing
             $resultSubset = (ConvertFrom-Json  $result.Content)
             $ReturnValue.AddRange($resultSubset.value)
-        }until([string]::IsNullOrEmpty($resultSubset.nextLink))
+        } until([string]::IsNullOrEmpty($resultSubset.nextLink))
     }
     catch {
         if ($_.Exception.Response.StatusCode -eq "Forbidden") {
             $errorMessage = "Something went wrong $($_.ScriptStackTrace). Error message: '$($_.Exception.Message)'"
         } elseif (![string]::IsNullOrEmpty($_.ErrorDetails.Message)) {
-            $errorMessage = "Something went wrong $($_.ScriptStackTrace). Error message: '$($_.ErrorDetails.Message)'" 
+            $errorMessage = "Something went wrong $($_.ScriptStackTrace). Error message: '$($_.ErrorDetails.Message)'"
         } else {
-            $errorMessage = "Something went wrong $($_.ScriptStackTrace). Error message: '$($_)'" 
-        }  
+            $errorMessage = "Something went wrong $($_.ScriptStackTrace). Error message: '$($_)'"
+        }
         throw $errorMessage
     }
     return $ReturnValue
 }
 
-function Get-RaetPersonDataList { 
+function Get-RaetPersonDataList {
     [CmdletBinding()]
     param ()
-    
+
     $Script:BaseUrl = "https://api.raet.com/iam/v1.0"
-    
+
     try {
         $ActiveCompareDate = Get-Date
 
@@ -126,7 +125,7 @@ function Get-RaetPersonDataList {
         $jobProfiles = $jobProfiles | Select-Object * -ExcludeProperty extensions
         $jobProfiles = $jobProfiles | Where-Object {$_.isActive -ne $false}
         $jobProfiles = $jobProfiles | Where-Object {
-                ($ActiveCompareDate -ge ([Datetime]::ParseExact($_.validFrom, 'yyyy-MM-dd', $null))) -and 
+                ($ActiveCompareDate -ge ([Datetime]::ParseExact($_.validFrom, 'yyyy-MM-dd', $null))) -and
                 (([Datetime]::ParseExact($_.validUntil, 'yyyy-MM-dd', $null)) -ge $ActiveCompareDate)
         }
         $jobProfileGrouped = $jobProfiles | Group-Object Id -AsHashTable
@@ -142,11 +141,10 @@ function Get-RaetPersonDataList {
                         $assignmentHashtable.$tmpKey += ($record)
                     } else {
                         $assignmentHashtable.Add($tmpKey, @($record))
-                    } 
+                    }
                 }
-            }   
+            }
         }
-      
 
         # Extend the persons model
         $persons | Add-Member -MemberType NoteProperty -Name "BusinessEmailAddress" -Value $null -Force
@@ -154,8 +152,8 @@ function Get-RaetPersonDataList {
         $persons | Add-Member -MemberType NoteProperty -Name "BusinessPhoneNumber" -Value $null -Force
         $persons | Add-Member -MemberType NoteProperty -Name "MobilePhoneNumber" -Value $null -Force
         $persons | Add-Member -MemberType NoteProperty -Name "HomePhoneNumber" -Value $null -Force
-        
-        foreach ($person in $persons) { 
+
+        foreach ($person in $persons) {
             #Validate the required person fields
 
             $person | Add-Member -Name "ExternalId" -MemberType NoteProperty -Value $person.personCode
@@ -168,8 +166,8 @@ function Get-RaetPersonDataList {
             $person | Add-Member -Name "DisplayName" -MemberType NoteProperty -Value $displayName
 
             $contracts = New-Object System.Collections.Generic.List[System.Object]
-            foreach ($employment in $person.employments) {                
-                $jobProfile = $jobProfileGrouped["$($employment.jobProfile)"]    
+            foreach ($employment in $person.employments) {
+                $jobProfile = $jobProfileGrouped["$($employment.jobProfile)"]
 
                 if($true -eq $includeAssignments){
                     # Create Contract object(s) based on assignments
@@ -179,7 +177,7 @@ function Get-RaetPersonDataList {
                     $personAssignments = $assignmentHashtable.$lookingFor
                     foreach($assignment in $personAssignments){
                         if ($assignment.employmentCode -eq $employment.employmentCode) {
-                            $jobProfile = $jobProfileGrouped["$($assignment.jobProfile)"]                                                                                    
+                            $jobProfile = $jobProfileGrouped["$($assignment.jobProfile)"]
 
                             #Contract result object used in HelloID
                             $Contract = [PSCustomObject]@{
@@ -210,7 +208,7 @@ function Get-RaetPersonDataList {
                             }
                             $contracts.add($Contract)
                         }
-                    } 
+                    }
                 }else{
                     # Create Contract object(s) based on employments
 
@@ -251,10 +249,10 @@ function Get-RaetPersonDataList {
                     if (![string]::IsNullOrEmpty($emailAddress)) {
                         if ($emailAddress.type -eq "Business") {
                             $person.BusinessEmailAddress = $emailAddress.address
-                        } 
+                        }
                         if ($emailAddress.type -eq "Private") {
                             $person.PrivateEmailAddress = $emailAddress.address
-                        }                           
+                        }
                     }
                 }
 
@@ -269,7 +267,7 @@ function Get-RaetPersonDataList {
                         }
                         if ($phoneNumber.type -eq "Home") {
                             $person.HomePhoneNumber = $phoneNumber.number
-                        }       
+                        }
                     }
                 }
 
@@ -305,7 +303,7 @@ function Get-RaetPersonDataList {
     }
     catch {
         Throw "Could not Get-RaetPersonDataList, message: $($_.Exception.Message)"
-    } 
+    }
 }
 
 #call the Get-RaetPersonDataList function to get the data from the API
